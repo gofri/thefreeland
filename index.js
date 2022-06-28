94a369e0a6ee368a9c90031cc6027e33ae255db5
Gal Ofri <galmenashofri@gmail.com>
2022-06-28 20:46:22 +0300

    fix back

--- index.js
+++ index.js
@@ -18,14 +18,11 @@ app.get('*', async function(req, res) {
 	  res.send("")
 	  return
   }
-  /*
   url = "https://www.haaretz.co.il" + req.url
   d1 = new Date()
   html = await foo(url)
   d2 = new Date()
   console.log('total time', d2-d1)
-  */
-  html = "Express on Vercel: " + req.url
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(html)
